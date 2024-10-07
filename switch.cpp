ba4786a0aecf5a8a1fabe590bc2708a6f735f572
AyaanG123 <160043446+AyaanG123@users.noreply.github.com>
2024-10-07 08:58:59 -0400

    Update switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -1,10 +1,14 @@
+//Ayaan Gill
+//10/7/24
+//Switch Case
+//Extra: Extra emotion
 using namespace std;
 #include <iostream>
 
 int main() {
     int x = 5;
-    std::cout << "Enter your emotion\n1. Happy\n2. Sad\n3. Mad\n4. Rizzy\n5. Sigma\n";
-    cin >> x;
+    std::cout << "Enter your emotion\n1. Happy\n2. Sad\n3. Mad\n4. Rizzy\n5. Sigma\n6. Change your emotion to happy\n";
+    cin >> x;//user input
     
     switch (x) {
     case 1:
@@ -12,7 +16,7 @@ int main() {
         break;
     case 2:
         cout << "boooo";
-        break;
+        break;//depending on the input a different thing prints
     case 3:
         cout << "dont crashout";
         break;
@@ -22,9 +26,12 @@ int main() {
     case 5:
         cout << "what the sigma";
         break;
+    case 6:
+        cout << "ur emotion changed to happy";
+        break;
     default:
         cout << "That is not a valid emotion";
-        break;
+        break;//if input does not match anything, this prints
     }
 
     return 0;
